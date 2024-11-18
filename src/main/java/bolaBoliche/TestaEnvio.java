2e50da60bde8f91a04c7cbfcadfe8d5f4f290a21
luiz.coelho <uizhenrique@gmail.com>
2024-11-18 14:47:03 -0400

    Comentários adicionados e mensagem de erro complementada

--- src/main/java/bolaBoliche/TestaEnvio.java
+++ src/main/java/bolaBoliche/TestaEnvio.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class TestaEnvio {
     public static void main(String[] args) throws Exception {
     	try {
+    		
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Digite o diâmetro da bola\n");
+        /*A cada  medida a ser inserida, uma função de validação é chamada, lendo o input obtido pelo scanner*/
         int diametro;
 		diametro = validaNumero(sc.nextInt());
 	
 
+		//A mesma validação ocorre nas medidas da caixa
         System.out.println("Digite a altura, largura e profundidade da caixa no formato: X Y Z \n");
         int altura = validaNumero(sc.nextInt());
         int largura = validaNumero(sc.nextInt());
         int profundidade = validaNumero(sc.nextInt());
 
+        //Como os testes são automaticos, segui a risca as saídas, apenas com S ou N
         if (diametro <= altura && diametro <= largura && diametro <= profundidade) {
         	System.out.println("S"); 
         } else {
@@ -24,6 +29,7 @@ public class TestaEnvio {
 
         sc.close();
     	}
+    	//Caso alguma validação lance a exception, o programa reinicia.
     	catch(Exception e) {
     		e.printStackTrace();
     		
@@ -32,13 +38,13 @@ public class TestaEnvio {
       
         
     }
-    
+   
     public static int validaNumero(int numero) throws Exception {
     	if (numero >= 1 && numero <= 10000) {
 			return numero;
 		}else 
 		{
-			throw new Exception("Numero inválido");
+			throw new Exception("Numero inválido, digite um número no formato inteiro, e de tamanho válido (1 <= X <= 10.000)");
 		}
     	
     }
