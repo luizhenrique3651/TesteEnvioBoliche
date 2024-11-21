c41cf8efd77f139c900af277367195b934bed43b
luiz.coelho <uizhenrique@gmail.com>
2024-11-21 13:48:56 -0400

    Classe de start para o springboot

---
+++ src/main/java/com/buson/BolaBolicheApplication.java
@@ -0,0 +1,13 @@
+package com.buson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaBolicheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaBolicheApplication.class, args);
+	}
+
+}
